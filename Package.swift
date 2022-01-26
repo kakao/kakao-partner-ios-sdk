9b97df07b3de2193f08de178525a3e5c02665d58
kakaosdk <kakaosdk.dev@kakaocorp.com>
2022-01-26 16:01:41 +0900

    2.8.5

---
+++ Package.swift
@@ -0,0 +1,78 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "KakaoPartnerSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "KakaoPartnerSDK",
+            targets: ["KakaoPartnerSDKCommon", "KakaoPartnerSDKAuth", "KakaoPartnerSDKUser", "KakaoPartnerSDKTalk", "KakaoPartnerSDKLink"]),
+        .library(
+            name: "KakaoPartnerSDKCommon",
+            targets: ["KakaoPartnerSDKCommon"]),
+        .library(
+            name: "KakaoPartnerSDKAuth",
+            targets: ["KakaoPartnerSDKAuth"]),
+        .library(
+            name: "KakaoPartnerSDKUser",
+            targets: ["KakaoPartnerSDKUser"]),
+        .library(
+            name: "KakaoPartnerSDKTalk",
+            targets: ["KakaoPartnerSDKTalk"]),
+        .library(
+            name: "KakaoPartnerSDKLink",
+            targets: ["KakaoPartnerSDKLink"])
+    ],
+    dependencies: [
+        .package(name: "KakaoOpenSDK",
+                 url: "https://github.com/kakao/kakao-ios-sdk.git", .branch("master"))
+    ],
+    targets: [
+        .target(
+            name: "KakaoPartnerSDKCommon",
+            dependencies: [
+                .product(name: "KakaoSDKCommon", package: "KakaoOpenSDK"),
+            ],
+            exclude: ["Info.plist"]
+        ),
+        .target(
+            name: "KakaoPartnerSDKAuth",
+            dependencies: [
+                .target(name: "KakaoPartnerSDKCommon"),
+                .product(name: "KakaoSDKAuth", package: "KakaoOpenSDK"),
+            ],
+            exclude: ["Info.plist"]
+        ),
+        .target(
+            name: "KakaoPartnerSDKUser",
+            dependencies: [
+                .target(name: "KakaoPartnerSDKAuth"),
+                .product(name: "KakaoSDKUser", package: "KakaoOpenSDK"),
+            ],
+            exclude: ["Info.plist"]
+        ),
+        .target(
+            name: "KakaoPartnerSDKTalk",
+            dependencies: [
+                .target(name: "KakaoPartnerSDKUser"),
+                .product(name: "KakaoSDKTalk", package: "KakaoOpenSDK"),
+            ],
+            exclude: ["Info.plist"]
+        ),
+        .target(
+            name: "KakaoPartnerSDKLink",
+            dependencies: [
+                .target(name: "KakaoPartnerSDKCommon"),
+                .product(name: "KakaoSDKLink", package: "KakaoOpenSDK"),
+            ],
+            exclude: ["Info.plist"]
+        )
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
